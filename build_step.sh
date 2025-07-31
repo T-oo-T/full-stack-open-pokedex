3beb76609dc696275110e567d3eee1f2f4d7bf02
Otto Peräkylä <otto.perakyla@gmail.com>
2025-07-31 09:51:37 +0300

    add build and start scripts

---
+++ build_step.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Build script"
+
+# add the commands here
+npm run build
+
+echo "Build script finished successfully!"
---
+++ start_step.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Start script"
+
+# add the commands here
+npm run start-prod
