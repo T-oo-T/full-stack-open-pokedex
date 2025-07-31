abe455cc798ddbbcaace7219b2bdcdb38bc1e37e
Otto Peräkylä <otto.perakyla@gmail.com>
2025-07-31 10:43:12 +0300

    test broken health check

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.get('/version', (_req, res) => {
 })
 
 app.get('/health', (_req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
 
